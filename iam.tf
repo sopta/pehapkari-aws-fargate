9a7f4b4ca8456f65e6df7f24e29b5c0f11a62201
zdeneksotolar <sotolar.zdenek@gmail.com>
2019-05-14 14:53:59 +0200

    remote IAM, set live template

--- iam.tf
+++ iam.tf
@@ -1,10 +1,1 @@
-resource "aws_iam_role" "ecs_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = "${file("policies/ecs-task-execution-role.json")}"
-}
-
-resource "aws_iam_role_policy" "ecs_execution_role_policy" {
-  name   = "ecsTaskExecutionRolePolicy"
-  policy = "${file("policies/ecs-task-execution-role-policy.json")}"
-  role   = "${aws_iam_role.ecs_execution_role.id}"
-}
+# IAM here (ecs_iam)
